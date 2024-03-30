b9f80564b06b6b0c7c74e5859adb5c6f98fcba98
rcwoshimao <mantoumiaoshen@gmail.com>
2024-03-30 00:55:50 -0700

    Added Images, changed text style, learned debugging

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableWithoutFeedback, Image } from 'react-native';
 
 export default function App() {
+  let x=1; 
+  console.log("Now try this.");
+  console.log("App Executed! Hellow! ");
+  const handlePress = () => console.log("Text pressed!!! THIS IS A LOG MESSAGE! WHY ARE YOU NOT LOGGING BRUH!! PLEASEE LOG!! ");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Hello World!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={10} onPress={handlePress}>Hello World! </Text>
+      <TouchableWithoutFeedback>
+      <Image 
+      blurRadius={2}
+      fadeDuration={1000}
+      source={{
+        width: 200, 
+        height: 300,
+        uri:"https://picsum.photos/200/300"
+      }}/>
+      </TouchableWithoutFeedback>
 
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FFF',
+    justifyContent: "center",
+    alignItems: "center",
+    fontSize: 100,
   },
 });
